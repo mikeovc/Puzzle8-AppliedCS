abbd027adc7b7324a4a01543021ea7d63b640137
Mikhail Ovchinnikov <mikeovc@gmail.com>
2016-09-16 01:53:25 -0700

    Mostly finished, initial commit

---
+++ app/src/main/java/com/google/engedu/puzzle8/PuzzleBoard.java
@@ -0,0 +1,181 @@
+package com.google.engedu.puzzle8;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Shader;
+
+import java.util.ArrayList;
+import java.lang.Math;
+
+
+public class PuzzleBoard {
+
+    private static final int NUM_TILES = 3;
+    private static final int[][] NEIGHBOUR_COORDS = {
+            { -1, 0 },
+            { 1, 0 },
+            { 0, -1 },
+            { 0, 1 }
+    };
+    private ArrayList<PuzzleTile> tiles = new ArrayList<>();
+    private int steps = 0;
+    private PuzzleBoard previousBoard;
+
+    PuzzleBoard(Bitmap bitmap, int parentWidth) {
+        bitmap = bitmap.createScaledBitmap(bitmap, parentWidth, parentWidth, false);
+        int tileWidthAndHeight = bitmap.getWidth() / NUM_TILES;
+        for (int i = 0; i < NUM_TILES; i++) {
+            for (int j = 0; j < NUM_TILES; j++) {
+                int yStart = i * tileWidthAndHeight;
+                int xStart = j * tileWidthAndHeight;
+                Bitmap b = bitmap.createBitmap(bitmap, xStart, yStart, tileWidthAndHeight, tileWidthAndHeight);
+                if (i == NUM_TILES - 1 && j == NUM_TILES -1) {
+                    tiles.add(null);
+                } else {
+                    tiles.add(new PuzzleTile(b, (i * NUM_TILES) + j));
+                }
+            }
+        }
+    }
+
+    PuzzleBoard(PuzzleBoard otherBoard) {
+        steps = otherBoard.steps + 1;
+        previousBoard = otherBoard;
+        tiles = (ArrayList<PuzzleTile>) otherBoard.tiles.clone();
+    }
+
+    public PuzzleBoard getPreviousBoard() { return previousBoard; }
+    public void setPreviousBoard(PuzzleBoard p) { previousBoard = p; }
+
+    public void reset() {
+        // Nothing for now but you may have things to reset once you implement the solver.
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null)
+            return false;
+        return tiles.equals(((PuzzleBoard) o).tiles);
+    }
+
+    public void draw(Canvas canvas) {
+        if (tiles == null) {
+            return;
+        }
+        for (int i = 0; i < NUM_TILES * NUM_TILES; i++) {
+            PuzzleTile tile = tiles.get(i);
+            if (tile != null) {
+                tile.draw(canvas, i % NUM_TILES, i / NUM_TILES);
+            }
+        }
+    }
+
+    public boolean click(float x, float y) {
+        for (int i = 0; i < NUM_TILES * NUM_TILES; i++) {
+            PuzzleTile tile = tiles.get(i);
+            if (tile != null) {
+                if (tile.isClicked(x, y, i % NUM_TILES, i / NUM_TILES)) {
+                    return tryMoving(i % NUM_TILES, i / NUM_TILES);
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean tryMoving(int tileX, int tileY) {
+        for (int[] delta : NEIGHBOUR_COORDS) {
+            int nullX = tileX + delta[0];
+            int nullY = tileY + delta[1];
+            if (nullX >= 0 && nullX < NUM_TILES && nullY >= 0 && nullY < NUM_TILES &&
+                    tiles.get(XYtoIndex(nullX, nullY)) == null) {
+                swapTiles(XYtoIndex(nullX, nullY), XYtoIndex(tileX, tileY));
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public boolean resolved() {
+        for (int i = 0; i < NUM_TILES * NUM_TILES - 1; i++) {
+            PuzzleTile tile = tiles.get(i);
+            if (tile == null || tile.getNumber() != i)
+                return false;
+        }
+        return true;
+    }
+
+    private int XYtoIndex(int x, int y) {
+        return x + y * NUM_TILES;
+    }
+
+    protected void swapTiles(int i, int j) {
+        PuzzleTile temp = tiles.get(i);
+        tiles.set(i, tiles.get(j));
+        tiles.set(j, temp);
+    }
+
+    public ArrayList<PuzzleBoard> neighbours() {
+        ArrayList<PuzzleBoard> neighbours = new ArrayList<>();
+
+        int indexOfEmptyTile;
+        indexOfEmptyTile = tiles.indexOf(null);
+
+        int emptyX = indexOfEmptyTile % NUM_TILES;
+        int emptyY = indexOfEmptyTile / NUM_TILES;
+        int tileX, tileY;
+        for (int[] delta : NEIGHBOUR_COORDS) {
+            tileX = emptyX + delta[0];
+            tileY = emptyY + delta[1];
+            // If move is within bounds:
+            if ( (tileX >= 0 && tileX < NUM_TILES) && (tileY >= 0 && tileY < NUM_TILES) ) {
+                PuzzleBoard p = new PuzzleBoard(this);
+                p.tryMoving(tileX, tileY);
+                neighbours.add(p);
+            }
+        }
+
+        return neighbours;
+    }
+
+    public int priority() {
+        int tileX, tileY, desiredX, desiredY;
+        int manhattanDistance = 0;
+        for (int i = 0; i < tiles.size(); i++) {
+            if (tiles.get(i) != null) {
+                tileX = i % NUM_TILES;
+                tileY = i / NUM_TILES;
+                desiredX = tiles.get(i).getNumber() % NUM_TILES;
+                desiredY = tiles.get(i).getNumber() / NUM_TILES;
+                manhattanDistance += Math.abs(tileX - desiredX) + Math.abs(tileY - desiredY);
+            }
+        }
+        return manhattanDistance + steps;
+    }
+
+    public ArrayList<PuzzleBoard> allPreviousBoards() {
+        ArrayList<PuzzleBoard> previousBoards = new ArrayList<>();
+        PuzzleBoard b = previousBoard;
+        previousBoards.add(this);
+        while (b != null) {
+            previousBoards.add(b);
+            b = b.previousBoard;
+        }
+        return previousBoards;
+    }
+
+    public boolean sameStateAs(PuzzleBoard otherBoard) {
+        for (int i = 0; i < tiles.size(); i++) {
+            if (tiles.get(i) != null && otherBoard.tiles.get(i) != null) {
+                if (tiles.get(i).getNumber() != otherBoard.tiles.get(i).getNumber()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void clearHistory() {
+        previousBoard = null;
+    }
+}
---
+++ app/src/main/java/com/google/engedu/puzzle8/PuzzleBoardView.java
@@ -0,0 +1,129 @@
+package com.google.engedu.puzzle8;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+public class PuzzleBoardView extends View {
+    public static final int NUM_SHUFFLE_STEPS = 40;
+    static final Comparator<PuzzleBoard> COMPARATOR = new Comparator<PuzzleBoard>() {
+        @Override
+        public int compare(PuzzleBoard puzzleBoard, PuzzleBoard t1) {
+            if (puzzleBoard.priority() < t1.priority()) {
+                return -1;
+            }
+            else if (puzzleBoard.priority() > t1.priority()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    };
+
+    private Activity activity;
+    private PuzzleBoard puzzleBoard;
+    private ArrayList<PuzzleBoard> animation;
+    private Random random = new Random();
+
+    public PuzzleBoardView(Context context) {
+        super(context);
+        activity = (Activity) context;
+        animation = null;
+    }
+
+    public void initialize(Bitmap imageBitmap) {
+        int width = getWidth();
+        puzzleBoard = new PuzzleBoard(imageBitmap, width);
+        refreshScreen();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (puzzleBoard != null) {
+            if (animation != null && animation.size() > 0) {
+                puzzleBoard = animation.remove(0);
+                puzzleBoard.draw(canvas);
+                if (animation.size() == 0) {
+                    animation = null;
+                    puzzleBoard.reset();
+                    Toast toast = Toast.makeText(activity, "Solved! ", Toast.LENGTH_LONG);
+                    toast.show();
+                } else {
+                    this.postInvalidateDelayed(500);
+                }
+            } else {
+                puzzleBoard.draw(canvas);
+            }
+        }
+    }
+
+    public void shuffle() {
+        if (animation == null && puzzleBoard != null) {
+            for (int i = 0; i < NUM_SHUFFLE_STEPS; i++) {
+                ArrayList<PuzzleBoard> boards = puzzleBoard.neighbours();
+                int randomIndex = random.nextInt(boards.size());
+                puzzleBoard = boards.get(randomIndex);
+            }
+            refreshScreen();
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (animation == null && puzzleBoard != null) {
+            switch(event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    if (puzzleBoard.click(event.getX(), event.getY())) {
+                        invalidate();
+                        if (puzzleBoard.resolved()) {
+                            Toast toast = Toast.makeText(activity, "Congratulations!", Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                        return true;
+                    }
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+
+    public void solve() {
+        PriorityQueue<PuzzleBoard> boards = new PriorityQueue<>(1, COMPARATOR);
+        boards.add(puzzleBoard);
+
+        while (boards.size() != 0) {
+            PuzzleBoard retrievedBoard = boards.poll();
+            if (!retrievedBoard.resolved()) {
+                for (PuzzleBoard neighbour : retrievedBoard.neighbours()) {
+                    if (retrievedBoard.getPreviousBoard() == null ||
+                            !neighbour.sameStateAs(retrievedBoard.getPreviousBoard())) {
+                        neighbour.setPreviousBoard(retrievedBoard);
+                        boards.add(neighbour);
+                    }
+                }
+            } else {
+                boards.clear();
+                ArrayList<PuzzleBoard> solvePath = retrievedBoard.allPreviousBoards();
+                Collections.reverse(solvePath);
+                retrievedBoard.clearHistory();
+                animation = solvePath;
+                invalidate();
+            }
+        }
+    }
+
+    private void refreshScreen() {
+        puzzleBoard.reset();
+        invalidate();
+    }
+}
