49ac0041b410d78dd39f8428c9986c2368e484f1
Mikhail Ovchinnikov <mikeovc@gmail.com>
2016-09-16 20:22:37 -0700

    change pic taking to higher quality

--- app/src/main/java/com/google/engedu/puzzle8/PuzzleActivity.java
+++ app/src/main/java/com/google/engedu/puzzle8/PuzzleActivity.java
@@ -3,21 +3,30 @@ package com.google.engedu.puzzle8;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import java.io.File;
+import java.io.IOException;
 
 public class PuzzleActivity extends AppCompatActivity {
 
+    private static final String TAG = "PuzzleActivity";
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private Bitmap imageBitmap = null;
     private PuzzleBoardView boardView;
+    private Uri photoURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +37,8 @@ public class PuzzleActivity extends AppCompatActivity {
         RelativeLayout container = (RelativeLayout) findViewById(R.id.puzzle_container);
         boardView = new PuzzleBoardView(this);
         // Some setup of the view.
-        boardView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+        boardView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                                                                  RelativeLayout.LayoutParams.MATCH_PARENT));
         container.addView(boardView);
     }
 
@@ -57,18 +67,36 @@ public class PuzzleActivity extends AppCompatActivity {
     public void dispatchTakePictureIntent(View view) {
         Intent takePicIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePicIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePicIntent, REQUEST_IMAGE_CAPTURE);
+            File photo = null;
+            try {
+                photo = createImageFile();
+            }
+            catch (IOException ex) {
+                Log.v(TAG, "Couldn't create File photo :", ex);
+            }
+            if (photo != null) {
+                photoURI = Uri.fromFile(photo);
+                takePicIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePicIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
-            // Resize to a square without scaling
-            imageBitmap = Bitmap.createBitmap(imageBitmap, 0, 0, imageBitmap.getWidth(), imageBitmap.getWidth());
+            try {
+                imageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), photoURI);
+                imageBitmap = rotateImageIfRequired(imageBitmap, photoURI);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            // Resize to a square before scaling in boardView
+            imageBitmap = Bitmap.createBitmap(imageBitmap, 0, 0, imageBitmap.getWidth(),
+                                              imageBitmap.getWidth());
             boardView.initialize(imageBitmap);
+            deletePicHistory();
         }
     }
 
@@ -83,5 +111,53 @@ public class PuzzleActivity extends AppCompatActivity {
     public void useStockPic(View view) {
         imageBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.skelly);
         boardView.initialize(imageBitmap);
+    }
+
+    private File createImageFile() throws IOException {
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile("photo", ".jpg", storageDir);
+    }
+
+    // Some phones store image in landscape, some in portrait.
+    // In either case, rotate them appropriately. Code taken from StackOverflow.
+    private Bitmap rotateImageIfRequired(Bitmap image, Uri imageUri) throws IOException {
+        ExifInterface ei = new ExifInterface(imageUri.getPath());
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                                             ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotateImage(image, 90);
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotateImage(image, 180);
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotateImage(image, 270);
+            default:
+                return image;
+        }
+    }
+
+    private Bitmap rotateImage(Bitmap image, int degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        Bitmap rotatedImage = Bitmap.createBitmap(image, 0, 0, image.getWidth(),
+                                                  image.getHeight(), matrix, true);
+        image.recycle();
+        return rotatedImage;
+    }
+
+    // Make sure photos aren't being needlessly persistently stored in phone storage:
+    private void deletePicHistory() {
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if (dir == null) { return; }
+        String path = dir.toString();
+        Log.d("Files", "Path: " + path);
+        File f = new File(path);
+        File files[] = f.listFiles();
+        Log.d("Files", "Size: " + files.length);
+        for (File file : files) {
+            Log.d("Files", "FileName: " + file.getName());
+            file.delete();
+        }
     }
 }
